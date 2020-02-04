b4c263059474caf03c3e8b8226ddbdb7998a9f7f
Richard Marcoux <marcourp@morrisville.edu>
2020-02-04 15:42:12 -0500

    S2L9 - Print to Console with Debug.Log(); added a script and changed the name of the scene.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        
-    }
+        // Intro. to game
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number, but don't tell me what it is...");
+        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The lowest number you can pick is 1");
+    } // Start
 
     // Update is called once per frame
     void Update()
